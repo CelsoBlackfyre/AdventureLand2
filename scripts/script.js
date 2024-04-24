5fe51d0480606b7d47ae56ae9465c1814f23db19
Celso <celsomartinscarvalho@gmail.com>
2024-04-23 22:08:03 -0300

    aulafinalizada

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
 //SCRIPTS DO SITE
+
+//container-criar-conta
+//container-login
+
+function alternarFormulario() {
+	var pegarCadastro = document.getElementById("cadastro");
+	var pegarLogin = document.getElementById("login");
+
+	if (pegarCadastro.style.display === "none") {
+		pegarCadastro.style.display = "block";
+		pegarLogin.style.display = "none";
+	} else {
+		pegarCadastro.style.display = "none";
+		pegarLogin.style.display = "block";
+	}
+}
